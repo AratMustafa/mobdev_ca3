e08f024121d938537da37932ef3f9f5afbd492dd
snap-balance <2022141@student.cct.ie>
2022-11-15 15:31:17 +0000

    changes quote-details and page.ts

--- src/app/pages/quotes/quotes.page.ts
+++ src/app/pages/quotes/quotes.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { ApiService } from '../../services/api.service';
+import { NavController } from '@ionic/angular';
+import { ApiService } from '../..//services/api.service';
 
 @Component({
   selector: 'app-quotes',
@@ -11,15 +12,15 @@ import { ApiService } from '../../services/api.service';
 export class QuotesPage implements OnInit {
 
   quotes: Observable<any>;
-  constructor(private router: Router, private api: ApiService) { }
+  constructor(private navController: NavController, private router: Router, private api: ApiService) { }
 
   ngOnInit() {
     this.quotes = this.api.getQuotes();
   }
 
   openDetails(quote){
-    let split = quote.url.split('/');
-    let quoteId = split[split.length-2];
+    let quoteId = quote.quote_id;
     this.router.navigateByUrl(`/tabs/quotes/${quoteId}`);
   }
 }
+
