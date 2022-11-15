7e25f9ad10acbc54e3fd012e22a2930190c873b7
snap-balance <2022141@student.cct.ie>
2022-11-15 11:48:54 +0000

    changes characters.page.ts

--- src/app/pages/characters/characters.page.ts
+++ src/app/pages/characters/characters.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { ApiService } from '../../services/api.service';
+import { NavController } from '@ionic/angular';
+import { ApiService } from '../..//services/api.service';
 
 @Component({
   selector: 'app-characters',
@@ -11,15 +12,15 @@ import { ApiService } from '../../services/api.service';
 export class CharactersPage implements OnInit {
 
   characters: Observable<any>;
-  constructor(private router: Router, private api: ApiService) { }
+  constructor(private navController: NavController, private router: Router, private api: ApiService) { }
 
   ngOnInit() {
     this.characters = this.api.getCharacters();
   }
 
-  openDetails(character){
-    let split = character.url.split('/');
-    let characterId = split[split.length-2];
-    this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+  openDetails(characters){
+    let split = characters.url.split('/');
+    let charactersId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/characters/${charactersId}`);
   }
 }
